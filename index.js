cdf80f2dbcc9f2175787e41e36cafc3b1a0e2d46
Sachin Jain <sachinjain3677>
2020-04-13 21:10:47 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const port = 3000;
+
+server.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
+
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/javascript', (req, res) => {
+	res.sendFile(__dirname + '/public/javascript.html');
+});
+
+app.get('/swift', (req, res) => {
+	res.sendFile(__dirname + '/public/swift.html');
+});
+
+
+app.get('/css', (req, res) => {
+	res.sendFile(__dirname + '/public/css.html');
+});
+
+app.get('/chat', (req, res) => {
+	res.sendFile(__dirname + '/public/chat.html');
+});
+
+const tech = io.of('/tech');
+
+tech.on('connection' ,(socket) => {
+	// console.log('user connected');
+	socket.on('join', (data => {
+		socket.join(data.room);
+		tech.in(data.room).emit('message', `${data.username} joined ${data.room} room!`);
+	}))
+
+	socket.on('message', (data) => {
+		console.log(`${data.username}: ${data.msg}`);
+		tech.in(data.room).emit('message', `${data.username}: ${data.msg}`);
+	});
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+
+		tech.emit('message', 'user disconnected');
+	})
+})
