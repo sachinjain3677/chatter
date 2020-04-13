5572f4cbe6c27d3f5e751bc8b29a8db4e9aff7b1
Sachin Jain <sachinjain3677>
2020-04-13 21:17:36 +0530

    changed index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const port = 3000;
+const Process = require('process');
+const port = Process.env.PORT || 3000;
 
 server.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
